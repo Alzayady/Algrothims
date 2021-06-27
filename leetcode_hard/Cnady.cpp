655eff31dae5672f21ec5b742e6409816a333e95
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-06-27 20:02:04 +0200

    Create Cnady.cpp

---
+++ leetcode_hard/Cnady.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int candy(vector<int> ratings) {
+        priority_queue<pair<int, int >> pq;
+        int n = ratings.size();
+        for (int i = 0; i < n; i++) {
+            pq.push({ratings[i], i});
+        }
+        int ans = 0;
+        vector<bool> vis(n, 0);
+        while (pq.size()) {
+            pair<int, int> last = pq.top();
+            pq.pop();
+            if (vis[last.second])continue;
+            int need_left = 0;
+            int length_left = 0;
+            int need_right = 0;
+            int length_right = 0;
+            int it = last.second + 1;
+            while (it < n and ratings[it] < ratings[it - 1]) {
+                if (ratings[it] < ratings[it - 1]) {
+                    need_right += 1;
+                }
+                length_right += 1;
+                it++;
+            }
+            it = last.second - 1;
+            while (it >= 0 and ratings[it] < ratings[it + 1]) {
+                if (ratings[it] < ratings[it + 1]) {
+                    need_left += 1;
+                }
+                length_left += 1;
+                it--;
+            }
+            ans+= max(need_left, need_right) + 1;
+            int first_right = need_right;
+            for (int i = 0; i < length_right; i++) {
+                if (!vis[last.second + i + 1]) {
+                    ans += first_right;
+                    vis[last.second + i + 1] = 1;
+                }
+                    first_right--;
+            }
+            int first_left = need_left;
+            for (int i = 0; i < length_left; i++) {
+                if (!vis[last.second - i - 1]) {
+                    ans += first_left;
+                    vis[last.second - i - 1] = 1;
+                }
+                    first_left--;
+            }
+
+        }
+        return ans;
+    }
+};
+
