f7c2b882d0e888ddfcb2e2aec45dea0b794d6c99
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-06-28 12:36:14 +0200

    Update Cnady.cpp

--- leetcode_hard/Cnady.cpp
+++ leetcode_hard/Cnady.cpp
@@ -12,28 +12,20 @@ public:
             pair<int, int> last = pq.top();
             pq.pop();
             if (vis[last.second])continue;
-            int need_left = 0;
             int length_left = 0;
-            int need_right = 0;
             int length_right = 0;
             int it = last.second + 1;
             while (it < n and ratings[it] < ratings[it - 1]) {
-                if (ratings[it] < ratings[it - 1]) {
-                    need_right += 1;
-                }
                 length_right += 1;
                 it++;
             }
             it = last.second - 1;
             while (it >= 0 and ratings[it] < ratings[it + 1]) {
-                if (ratings[it] < ratings[it + 1]) {
-                    need_left += 1;
-                }
                 length_left += 1;
                 it--;
             }
-            ans+= max(need_left, need_right) + 1;
-            int first_right = need_right;
+            ans+= max(length_left, length_right) + 1;
+            int first_right = length_right;
             for (int i = 0; i < length_right; i++) {
                 if (!vis[last.second + i + 1]) {
                     ans += first_right;
@@ -41,7 +33,7 @@ public:
                 }
                     first_right--;
             }
-            int first_left = need_left;
+            int first_left = length_left;
             for (int i = 0; i < length_left; i++) {
                 if (!vis[last.second - i - 1]) {
                     ans += first_left;
