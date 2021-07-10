d5faca5fdf7a7b8b4197352ca95ff393785b80f5
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-07-10 11:08:20 +0200

    Update Decode Ways II.cpp

--- leetcode_hard/Decode Ways II.cpp
+++ leetcode_hard/Decode Ways II.cpp
@@ -1,50 +1,50 @@
 class Solution {
 public:
-    const static int N = 1e5+10;
-    const int MOD = 1e9+7;
-    int dp[N];
-    string s ;
-    int n ;
-    long long  solve(int index){
-        if(index == n)return 1;
-        if(index > n)return 0 ;
-        if(dp[index]!=-1){
+    vector<int> dp;
+    const int MOD = 1e9 + 7;
+    string s;
+    int n;
+
+    long long solve_one(int index) {
+        return solve(index + 1) + (s[index + 1] == '*' ? 9 : 1) * solve(index + 2);
+    }
+
+    long long solve_two(int index) {
+        long long ans = solve(index + 1);
+        if (s[index + 1] == '*') {
+            ans += 6 * solve(index + 2);
+        } else if (s[index + 1] <= '6') {
+            ans += solve(index + 2);
+        }
+        return ans;
+    }
+
+    long long solve(int index) {
+        if (index == n)return 1;
+        if (index > n)return 0;
+        if (dp[index] != -1) {
             return dp[index];
         }
-        long long  ans = 0;
-        if(s[index]=='0'){
-            return 0 ;
-        }else if(s[index]=='1'){
-            ans = solve(index+1) + (s[index+1] =='*' ? 9 : 1 ) * solve(index+2);
-            ans%=MOD;
-        }else if(s[index] == '2' ){
-             ans = solve(index+1);
-             if(s[index+1] == '*'){
-                 ans += 6 * solve(index+2);
-             }else if(s[index+1] <='6'){
-                 ans += solve(index+2);
-             }
-            ans%=MOD;
-        }else if(s[index]=='*'){
-            ans =  (s[index+1] =='*' ? 9 : 1 ) * solve(index+2);
-            ans%=MOD;
-             if(s[index+1] == '*'){
-                 ans += 6 * solve(index+2);
-             }else if(s[index+1] <='6'){
-                 ans += solve(index+2);
-             }
-            ans%=MOD;
-            ans+= 9 * solve(index + 1 );
-            ans%=MOD;
-        }else{
-            ans = solve(index+1);
+        long long ans = 0;
+        if (s[index] == '0') {
+            ans = 0 ;
+        } else if (s[index] == '1') {
+            ans = solve_one(index);
+        } else if (s[index] == '2') {
+            ans = solve_two(index);
+        } else if (s[index] == '*') {
+            ans = solve_one(index) + solve_two(index) + 7 * solve(index + 1);
+        } else {
+            ans = solve(index + 1);
         }
-        return dp[index] = ans;
+
+        return dp[index] = ans % MOD;
     }
+
     int numDecodings(string s) {
         this->s = s;
         this->n = s.size();
-        memset(dp,-1,sizeof dp);
+        dp.resize(n, -1);
         return solve(0);
     }
 };
