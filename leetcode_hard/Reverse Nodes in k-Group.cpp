6f4fe6d3ef375039373d734c36a4702b1590fe65
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-07-18 14:31:15 +0200

    Update Reverse Nodes in k-Group.cpp

--- leetcode_hard/Reverse Nodes in k-Group.cpp
+++ leetcode_hard/Reverse Nodes in k-Group.cpp
@@ -10,51 +10,54 @@
  */
 class Solution {
 public:
-    struct buffer{
-        ListNode * firstNode ;
-        ListNode * lastNode ;
-        ListNode * firstNextGroup;
+    struct buffer {
+        ListNode *firstNode;
+        ListNode *lastNode;
+        ListNode *firstNextGroup;
     };
-    int get_size(ListNode *t){
-        int ans = 0; 
-        while(t!=NULL){
+
+    int get_size(ListNode *t) {
+        int ans = 0;
+        while (t != NULL) {
             ans++;
             t = t->next;
         }
         return ans;
     }
-    buffer * rev(ListNode * node , int k ){
-        buffer * b = new buffer();
-        ListNode * first = node ;
-        ListNode * second = node->next;
+
+    buffer *rev(ListNode *node, int k) {
+        buffer *b = new buffer();
+        ListNode *first = node;
+        ListNode *second = node->next;
         first->next = NULL;
         b->lastNode = node;
-        while(--k){
-            ListNode * temp = second->next;
+        while (--k) {
+            ListNode *temp = second->next;
             second->next = first;
             first = second;
             second = temp;
         }
-      
+
         b->firstNextGroup = second;
         b->firstNode = first;
         return b;
     }
-    ListNode* reverseKGroup(ListNode* head, int k) {
+
+    ListNode *reverseKGroup(ListNode *head, int k) {
         int sz = get_size(head);
-        int r = sz / k ;
-        ListNode * ans =NULL ;
-        ListNode * lastprev = NULL;
-        while(r--){
-            buffer * b = rev(head,k);
-            if(ans == NULL){
+        int r = sz / k;
+        ListNode *ans = NULL;
+        ListNode *lastprev = NULL;
+        while (r--) {
+            buffer *b = rev(head, k);
+            if (ans == NULL) {
                 ans = b->firstNode;
-            }else{
+            } else {
                 lastprev->next = b->firstNode;
             }
             lastprev = b->lastNode;
             head = b->firstNextGroup;
-            
+
         }
         lastprev->next = head;
         return ans;
