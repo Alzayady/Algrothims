c1c35bd860d94b9a83b1ce8615be4626c90d3eab
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-07-27 00:26:04 +0200

    Update liner_modular.cpp

--- number theory/liner_modular.cpp
+++ number theory/liner_modular.cpp
@@ -1,11 +1,19 @@
-ll liner_modular(ll a, ll b, ll m) {
+vector<ll> liner_modular(ll a, ll b, ll m) {
     // a X == b  ( mod m )
     // a X - b = m * y
     // a X - m * y = b
     ll xx;
     ll yy;
-    bool f ;
-    ll g = linear_diophantine(a, m, b, xx, yy , f);
-    if(f)return xx ;  // has only # gcd solutions unlike dlp which has INF solutions  
-    return INF;
+    bool f;
+    ll g = linear_diophantine(a, m, b, xx, yy, f);
+    if (f) {
+        xx = (((xx % m) + m) % m);
+        vector<ll> ans;
+        for (int i = 0; i < g; i++) {
+            ans.push_back((xx + i * m / g) % m);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;  // has only # gcd solutions unlike dlp which has INF solutions
+    }
+    return {};
 }
