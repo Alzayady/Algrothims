b811ee48764c8ca985c89b4c379d82803ba7a0b2
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-07-27 00:21:35 +0200

    Update liner_modular.cpp

--- number theory/liner_modular.cpp
+++ number theory/liner_modular.cpp
@@ -6,7 +6,6 @@ ll liner_modular(ll a, ll b, ll m) {
     ll yy;
     bool f ;
     ll g = linear_diophantine(a, m, b, xx, yy , f);
-    if(f)return xx ;
+    if(f)return xx ;  // has only # gcd solutions unlike dlp which has INF solutions  
     return INF;
-
 }
