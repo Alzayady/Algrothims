a99f805d4bed50d09c20c681aa44646cbd92da73
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-07-27 00:19:28 +0200

    Create liner_modular.cpp

---
+++ number theory/liner_modular.cpp
@@ -0,0 +1,12 @@
+ll liner_modular(ll a, ll b, ll m) {
+    // a X == b  ( mod m )
+    // a X - b = m * y
+    // a X - m * y = b
+    ll xx;
+    ll yy;
+    bool f ;
+    ll g = linear_diophantine(a, m, b, xx, yy , f);
+    if(f)return xx ;
+    return INF;
+
+}
