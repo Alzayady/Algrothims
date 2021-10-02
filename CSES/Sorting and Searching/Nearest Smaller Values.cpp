16ad6e551b3a24e7d1b4fd91095733f86d2c6d55
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-10-02 21:12:29 +0200

    Create Nearest Smaller Values.cpp

---
+++ CSES/Sorting and Searching/Nearest Smaller Values.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+#define IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+mt19937 rng((unsigned long long) chrono::steady_clock::now().time_since_epoch().count());
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+#define int long long
+#define ordered_set tree<ll, null_type,less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update>
+// Finding the number of element  strictly less than k=4 o_set.order_of_key(4)
+
+int32_t main() {
+    IO;
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    stack<int> stk;
+    vector<int> ans(n, -1);
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        while (stk.size() and vec[stk.top()] > vec[i]) {
+            ans[stk.top()] = i;
+            stk.pop();
+        }
+        stk.push(i);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] + 1 << " ";
+    }
+    cout << endl;
+    return 0;
+}
