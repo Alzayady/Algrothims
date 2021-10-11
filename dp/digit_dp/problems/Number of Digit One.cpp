bd39595a62053dfccbc755feb6959681e7d0f105
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-10-11 06:28:32 +0200

    Create Number of Digit One.cpp

---
+++ dp/digit_dp/problems/Number of Digit One.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> nums;
+    pair<int , int > dp[10][2];
+    pair<int , int > solve(int index , bool smaller = 0 ){ // return all and ans 
+        if(index == nums.size())return {1,0};
+        if(dp[index][smaller].first!=-1){
+            return dp[index][smaller];
+        }
+        int limit = 0 ;
+        if(smaller){
+            limit = 9;
+        }else{
+            limit = nums[index];
+        }
+        pair<int, int > ans = {0,0} ;
+        for(int i = 0 ; i <= limit ; i++){
+            bool nextS = smaller ;
+            if(nextS==0 and i < limit )nextS = 1 ;
+            pair<int , int > ret= solve(index+1,nextS);
+            ans.first+= ret.first;
+            ans.second+=ret.second;
+            if(i==1){
+                ans.second+= ret.first;
+            }
+        }
+        return dp[index][smaller] = ans;
+    }
+    int countDigitOne(int n) {
+        while(n){
+            nums.push_back(n%10);
+            n/=10;
+        }   
+        reverse(nums.begin(),nums.end());
+        memset(dp,-1,sizeof dp);
+        return solve(0).second;
+        
+    }
+};
