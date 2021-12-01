2dd912cd807e076103059f9a6af369b95758aa94
Ahmed Alzayady <45363985+Alzayady@users.noreply.github.com>
2021-12-01 19:55:39 +0200

    Update hopcroftKarp.cpp

--- graph/graph matching/hopcroftKarp.cpp
+++ graph/graph matching/hopcroftKarp.cpp
@@ -8,30 +8,29 @@ struct hopcroftKarp {
      *   3 -  we add all augmented paths to matching ones using dfs , by using the parent of each node stored from the last step
      *
      *   ----- original algrothim storing all possible augmented path which may intersected so it has more complexity to remove them , but this one count only possible paths
-     *         without needing to remove anything 
+     *         without needing to remove anything
      */
     vector<vector<int>> g;
-    int n;
+    const static int n = 1000;
     vector<int> parent;
     vector<int> mat;
     vector<int> mat_rev;
     unordered_set<int> leftSet;
     unordered_set<int> matchedSet2;
 
-    hopcroftKarp(vector<vector<int>> g_, int n_, unordered_set<int> leftSet_) {
+    hopcroftKarp(vector<vector<int>> g_,  unordered_set<int> leftSet_) {
         g = g_;
-        n = n_;
         leftSet = leftSet_;
         mat.resize(n + 1, -1);
         mat_rev.resize(n + 1, -1);
-        parent.resize(n+1,-1);
+        parent.resize(n + 1, -1);
     }
 
     vector<int> bfs() {
         vector<int> ans;
         queue<int> q;
         unordered_set<int> vis;
-        for (int i  : leftSet) {
+        for (int i: leftSet) {
             if (mat[i] == -1) {
                 q.push(i);
                 vis.insert(i);
@@ -40,7 +39,7 @@ struct hopcroftKarp {
         while (q.size()) {
             int last = q.front();
             q.pop();
-            for (int next : g[last]) {
+            for (int next: g[last]) {
                 if (vis.count(next))continue;
                 vis.insert(next);
                 parent[next] = last;
@@ -77,7 +76,7 @@ struct hopcroftKarp {
     vector<pair<int, int >> match() {
         vector<int> augmentedPath = bfs();
         while (augmentedPath.size()) {
-            for (int endNode:augmentedPath) {
+            for (int endNode: augmentedPath) {
                 vector<int> path;
                 path.push_back(endNode);
                 while (parent[endNode] != -1) {
@@ -97,7 +96,7 @@ struct hopcroftKarp {
             augmentedPath = bfs();
         }
         vector<pair<int, int>> ans;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             if (mat[i] != -1) {
                 ans.push_back({i, mat[i]});
             }
